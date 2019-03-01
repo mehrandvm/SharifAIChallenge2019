5dbc21280a12101a41ac1df13228e9a21d23e373
ADNAN <adnan.ali.yari@gmail.com>
2019-03-01 20:15:14 +0330

    move

--- AIC19-Client-Java-1.1.1/src/client/AI.java
+++ AIC19-Client-Java-1.1.1/src/client/AI.java
@@ -43,25 +43,55 @@ public class AI
         Hero[] myHeroes = world.getMyHeroes();
         Hero[] oppHeroes = world.getOppHeroes() ;
         Cell[] objZone = world.getMap().getObjectiveZone() ;
-        int rowMax = Integer.MIN_VALUE ;
-        int rowMin = Integer.MAX_VALUE ;
-        int columnMax = Integer.MIN_VALUE ;
-        int columnMin = Integer.MAX_VALUE ;
-        // finding max & min of row & column
+        Cell[] respwnZone = world.getMap().getMyRespawnZone() ;
+        Cell firstCell = null ;
+        Cell secondCell = null ;
+        Cell thirdCell = null ;
+        Cell fourthCell = null ;
+        int minDis = Integer.MIN_VALUE ;
         for ( int i = 0 ; i < objZone.length ; ++i )
         {
-            if ( objZone[i].getRow() > rowMax ) rowMax = objZone[i].getRow() ;
-            if ( objZone[i].getRow() < rowMin ) rowMin = objZone[i].getRow() ;
-            if ( objZone[i].getColumn() > columnMax ) columnMax = objZone[i].getColumn() ;
-            if ( objZone[i].getColumn() < columnMin ) columnMin = objZone[i].getColumn() ;
+            if ( world.manhattanDistance( respwnZone[0] , objZone[i] ) < minDis )
+            {
+                firstCell = objZone[i] ;
+                minDis = world.manhattanDistance( respwnZone[0] , objZone[i] ) ;
+            }
         }
-        // default initialization
-        Direction[][] dirs = new  Direction[4][];
-        dirs[0] = world.getPathMoveDirections( myHeroes[0].getCurrentCell() , world.getMap().getCell( rowMax , ( columnMax + columnMin ) / 2 ) ) ;
-        dirs[1] = world.getPathMoveDirections( myHeroes[1].getCurrentCell() , world.getMap().getCell( rowMin , ( columnMax + columnMin ) / 2 ) ) ;
-        dirs[2] = world.getPathMoveDirections( myHeroes[2].getCurrentCell() , world.getMap().getCell( ( rowMax + rowMin ) / 2 , columnMax ) ) ;
-        dirs[3] = world.getPathMoveDirections( myHeroes[2].getCurrentCell() , world.getMap().getCell( ( rowMax + rowMin ) / 2 , columnMin ) ) ;
-        // dynamic replacement
+        minDis = Integer.MIN_VALUE ;
+        for ( int i = 0 ; i < objZone.length ; ++i )
+        {
+            if ( world.manhattanDistance( respwnZone[1] , objZone[i] ) < minDis )
+            {
+                secondCell = objZone[i] ;
+                minDis = world.manhattanDistance( respwnZone[1] , objZone[i] ) ;
+            }
+        }
+        minDis = Integer.MIN_VALUE ;
+        for ( int i = 0 ; i < objZone.length ; ++i )
+        {
+            if ( world.manhattanDistance( respwnZone[2] , objZone[i] ) < minDis )
+            {
+                thirdCell = objZone[i] ;
+                minDis = world.manhattanDistance( respwnZone[2] , objZone[i] ) ;
+            }
+        }
+        minDis = Integer.MIN_VALUE ;
+        for ( int i = 0 ; i < objZone.length ; ++i )
+        {
+            if ( world.manhattanDistance( respwnZone[3] , objZone[i] ) < minDis )
+            {
+                fourthCell = objZone[i] ;
+                minDis = world.manhattanDistance( respwnZone[3] , objZone[i] ) ;
+            }
+        }
+        Direction[] dir_1 = world.getPathMoveDirections( myHeroes[0].getCurrentCell() , firstCell ) ;
+        Direction[] dir_2 = world.getPathMoveDirections( myHeroes[1].getCurrentCell() , secondCell ) ;
+        Direction[] dir_3 = world.getPathMoveDirections( myHeroes[2].getCurrentCell() , thirdCell ) ;
+        Direction[] dir_4 = world.getPathMoveDirections( myHeroes[3].getCurrentCell() , fourthCell ) ;
+        if ( dir_1.length > 0 ) world.moveHero( myHeroes[0] , dir_1[0] ) ;
+        if ( dir_2.length > 0 ) world.moveHero( myHeroes[1] , dir_1[0] ) ;
+        if ( dir_3.length > 0 ) world.moveHero( myHeroes[2] , dir_1[0] ) ;
+        if ( dir_4.length > 0 ) world.moveHero( myHeroes[3] , dir_1[0] ) ;
     }
 
     public void actionTurn(World world) {
@@ -86,5 +116,17 @@ public class AI
         /* ---- End of Action Logic Section ---- */
 
     }
-
+    static  Cell validCell( Cell cell , World world  , int count )
+    {
+        if ( cell.isInObjectiveZone() && cell.isWall() && count < 2 )
+        {
+            int row = cell.getRow() ;
+            int column = cell.getColumn() ;
+            validCell( world.getMap().getCell( row + 1 , column ) , world , count + 1 ) ;
+            validCell( world.getMap().getCell( row - 1 , column ) , world , count + 1 ) ;
+            validCell( world.getMap().getCell( row , column + 1 ) , world , count + 1 ) ;
+            validCell( world.getMap().getCell( row , column - 1 ) , world , count + 1 ) ;
+        }
+        return  cell ;
+    }
 }
